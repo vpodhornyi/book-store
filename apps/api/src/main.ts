260f36062fb87558e9bab0fd3a29f304d1d52893
Viktor <vpodhornyi@gmail.com>
2026-01-06 17:45:14 +0100

    add swager to main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Book store example')
+    .setDescription('The book store API description')
+    .setVersion('1.0')
+    .addTag('bookstore')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  if (process.env.NODE_ENV !== 'production') {
+    SwaggerModule.setup('api/docs', app, document);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
