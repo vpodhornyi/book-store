3132a557ed1fd7f3775d07aad8d01cdb25aa3959
Viktor <vpodhornyi@gmail.com>
2026-01-07 13:53:11 +0100

    - pino logger added to stdout - ref pino logs to clear format - global exception filter

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,59 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import type { Request } from 'express';
+import { LoggerModule } from 'nestjs-pino';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
+import { IncomingMessage, ServerResponse } from 'node:http';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 import { BooksModule } from './books/books.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { RequestIdMiddleware } from './common/middleware/request-id.middleware';
 
 @Module({
   imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.LOG_LEVEL ?? 'info',
+        genReqId: (req: Request) => req.requestId,
+        customProps: (req: Request) => ({
+          requestId: req.requestId,
+        }),
+        serializers: {
+          req(req: IncomingMessage) {
+            const r = req as IncomingMessage & {
+              id?: string;
+              method?: string;
+              url?: string;
+            };
+
+            return {
+              id: r.id,
+              method: r.method,
+              url: r.url,
+            };
+          },
+          res(res: ServerResponse) {
+            return {
+              statusCode: res.statusCode,
+            };
+          },
+        },
+        transport:
+          process.env.NODE_ENV === 'development'
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  colorize: true,
+                  singleLine: true,
+                  translateTime: 'HH:MM:ss',
+                  ignore: 'pid,hostname',
+                },
+              }
+            : undefined,
+      },
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
@@ -15,6 +62,10 @@ import { BooksModule } from './books/books.module';
     BooksModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HttpExceptionFilter],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestIdMiddleware).forRoutes('*');
+  }
+}
