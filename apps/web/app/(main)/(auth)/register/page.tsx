2eba85658e78f6aa6e5bd3a83f636997a2d73d11
Viktor <vpodhornyi@gmail.com>
2026-01-17 22:34:50 +0100

    login/register page fix

--- apps/web/app/(main)/(auth)/register/page.tsx
+++ apps/web/app/(main)/(auth)/register/page.tsx
@@ -1,28 +1,16 @@
-"use client";
-
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 import RegisterForm from "./register-form";
 
 export default function LoginModalPage() {
-  const router = useRouter();
 
   return (
-    <div
-      className="flex items-center justify-center"
-      onMouseDown={() => router.back()}
-    >
-      <div
-        className="w-full max-w-md p-6"
-        onMouseDown={(e) => e.stopPropagation()}
-      >
+    <div className="flex items-center justify-center">
+      <div className="w-full max-w-md p-6">
         <div className="mb-5 flex items-center justify-between">
           <h2 className="dark:text-gray-100 text-2xl font-semibold text-gray-800">Register</h2>
         </div>
-
         <RegisterForm/>
-
         <div className="dark:text-gray-300 mt-5 text-center text-sm text-black/60">
           Have an account?{" "}
           <Link href="/login" className="dark:text-gray-100 font-semibold text-black underline underline-offset-4">
